36fe5ff0fc182f07e60c1714b7cae8144d4007f3
Mahdi Tehrani <mahdi@frequencydesign.com>
2016-02-15 19:04:41 -0800

    Jedi Name - Node lesson 2

---
+++ 2-jedi-hq-node-routing/index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+
+app.get("/:firstname/:lastname", function(req, res) {
+	var firstName = req.params.firstname;
+	var lastName = req.params.lastname;
+	//res.send("Hello " + firstName + " " + lastName);
+	res.json({Person: firstName + " " + lastName});
+});
+
+app.get("/jedi/:firstname/:lastname", function(req, res) {
+	var firstName = req.params.firstname;
+	var lastName = req.params.lastname;
+	var jediName = lastName.slice(0, 3) + firstName.slice(0, 2)
+	//res.send("Hello Jedi " + jediName);
+	res.json({Jedi: jediName});
+});
+
+app.listen(3000, function() {
+	console.log("Example app listning on port 3000!");
+});
